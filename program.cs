136f7ac597354c65e2b1a6a7970f0a0f19319055
Rodrigo <rodrisaha6053@gmail.com>
2023-04-19 13:32:08 +0200

    Tarea terminada final 2

--- program.cs
+++ program.cs
@@ -1,7 +1,7 @@
 Console.WriteLine("Introduzca su nombre por favor");
 String nombre;
 nombre = Console.ReadLine();
-string reverse = "";
+String reverse = "";
 for (int i = nombre.Length - 1; i >= 0; i--)
 {
     reverse += nombre[i];
